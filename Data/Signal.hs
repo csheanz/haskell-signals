26d0a10584eb410228e8a9bc44930feee6b35ab7
csheanz <cameron_shea@hotmail.com>
2016-03-02 11:48:47 +1300

    Update Signal.hs
    Fixed module name.
    Fixed signature of withConnection.
    Use strict Map.
    Basic refactoring.

--- Data/Signal.hs
+++ Data/Signal.hs
@@ -1,4 +1,4 @@
-module Utils.Data.Signal
+module Data.Signal
     ( Signal()
     , Connection()
     , newSignal, connect, disconnect, block, unblock
@@ -13,7 +13,7 @@ import qualified Data.Foldable
 import Data.Maybe
 import qualified Data.Traversable
 import Data.IORef
-import qualified Data.Map as M
+import qualified Data.Map.Strict as M
 
 -- first slot value
 -- set to minBound::Int to double the number of ops required
@@ -21,21 +21,24 @@ import qualified Data.Map as M
 slot0 :: Integer
 slot0 = fromIntegral (minBound :: Int)
 
-newtype Counter = Counter (IORef Int)
-newCounter = Counter <$> newIORef 0
-inc (Counter ref) = atomicModifyIORef ref $ \i -> let j = succ i in (j, j)
-dec (Counter ref) = atomicModifyIORef ref $ \i -> let j = pred i in (j, j)
-gtz (Counter ref) = (> 0) <$> readIORef ref
+newtype Counter = Counter' (IORef Int)
+newCounter = Counter' <$> newIORef 0
+inc (Counter' ref) = atomicModifyIORef ref $ \i -> let j = succ i in (j, j)
+dec (Counter' ref) = atomicModifyIORef ref $ \i -> let j = pred i in (j, j)
+gtz (Counter' ref) = (> 0) <$> readIORef ref
 
-data Slot a = Slot Counter a
-data Sig a = Sig Integer (M.Map Integer (Slot a))
+data Slot a = Slot' Counter a
+blocked (Slot' x _) = gtz x
+apply z (Slot' _ f) = z f
 
-addSig (Sig i m) a =
+data Sig a = Sig' Integer (M.Map Integer (Slot a))
+
+addSig (Sig' i m) a =
     let m' = M.insert i a m
         i' = succ i
-    in  (Sig i' m', i)
+    in  (Sig' i' m', i)
 
-delSig (Sig i m) x = Sig i (M.delete x m)
+delSig (Sig' i m) x = Sig' i (M.delete x m)
 
 newtype Signal a = Signal' (IORef (Sig a))
 
@@ -47,13 +50,13 @@ data Connection = Connection'
 
 newSignal :: IO (Signal a)
 newSignal = do
-    ref <- newIORef $ Sig slot0 M.empty
+    ref <- newIORef $ Sig' slot0 M.empty
     return $ Signal' ref
 
 connect :: Signal a -> a -> IO Connection
 connect (Signal' ref) a = do
     cnt <- newCounter
-    let slot = Slot cnt a
+    let slot = Slot' cnt a
     atomicModifyIORef ref $ \sig ->
         let (s', i) = addSig sig slot
             disconn = atomicModifyIORef ref $ \sig' -> (delSig sig' i, ())
@@ -61,8 +64,8 @@ connect (Signal' ref) a = do
             unblk = void $ dec cnt
         in  (s', Connection' disconn blk unblk)
 
-withConnection :: Signal a -> a -> IO a -> IO a
-withConnection sig a = bracket (connect sig a) (disconnect) . const
+withConnection :: Signal a -> a -> IO b -> IO b
+withConnection sig a = bracket (connect sig a) disconnect . const
 
 withBlock :: Connection -> IO a -> IO a
 withBlock c = bracket_ (block c) (unblock c)
@@ -72,24 +75,24 @@ withUnblock c = bracket_ (unblock c) (block c)
 
 emit :: (a -> IO b) -> Signal a -> IO [b]
 emit z (Signal' ref) = do
-    Sig _ m <- readIORef ref
-    go $ M.toList m
+    Sig' _ m <- readIORef ref
+    go $ M.elems m
     where
         go [] = return []
-        go ((_, Slot x f) : ss) = do
-            blocked <- gtz x
-            if blocked
+        go (s : ss) = do
+            blk <- blocked s
+            if blk
                 then go ss
                 else do
-                    a <- z f
-                    b <- go ss
-                    return (a:b)
+                    x <- apply z s
+                    xs <- go ss
+                    return (x:xs)
 
 emit_ :: (a -> IO b) -> Signal a -> IO ()
 emit_ z (Signal' ref) = do
-    Sig _ m <- readIORef ref
+    Sig' _ m <- readIORef ref
     Data.Foldable.mapM_ go m
     where
-        go (Slot x f) = do
-            blocked <- gtz x
-            when (not blocked) (z f >> return ())
+        go s = do
+            b <- blocked s
+            unless b $ void $ apply z s
